01ff55f6a1c2ac2439629adbaec13e5f00f036e9
gram_jhm <gusah009@naver.com>
2021-05-14 21:58:00 +0900

    환고리즘 단층사진 푸는중... (내일은 꼭!)

--- 2021-1/ZOH/11_ct/ct.cpp
+++ 2021-1/ZOH/11_ct/ct.cpp
@@ -7,16 +7,20 @@ using namespace std;
 
 int N;
 int d[8], r[8], rd[8], ld[8];
-int board[8][8] = {0};
+char board[8][8];
+int cnt = 0;
+bool flag = false;
+
+int dir[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
 void input()
 {
-
-  ifstream in("marathon.inp");
+  ifstream in("ct.inp");
   in >> N;
   FOR(n, N)
   {
     in >> d[n];
+    cnt += d[n];
   }
   FOR(n, N)
   {
@@ -31,42 +35,82 @@ void input()
     in >> ld[n];
   }
   in.close();
+
+  FOR(i, 8) {
+    FOR(j, 8) {
+      board[i][j] = '-';
+    }
+  }
 }
 
-void backtracking(int n, int d)
+void DFS(int i, int j, int c)
 {
-  switch (d)
-  {
-  case 0:
-    FOR(i, n)
-    {
-      board[]
-    }
-    break;
-  case 1:
-    break;
-  case 2:
-    break;
-  case 3:
-    break;
+  if (c == cnt) {
+    cout << '\n';
+    flag = true;
+    return;
   }
+  if (r[i] == 0 || d[j] == 0 || \ 
+      rd[i + (N - j - 1)] == 0 || ld[i + j] == 0) {
+    return;
+  }
+  FOR(i, N) {
+    FOR(j, N) {
+      cout << board[i][j] << ' ';
+    }
+    cout << '\n';
+  }
+  cout<< '\n';
+  r[i]--;
+  d[j]--;
+  rd[i + (N - j - 1)]--;
+  ld[i + j]--;
+  board[i][j] = 'B';
+  // cout << i << " " << j << board[i][j] << '\n';
+  FOR(d, 4) {
+    int next_i = i + dir[d][0];
+    int next_j = j + dir[d][1];
+    if (0 < next_i && next_i < N && \
+        0 < next_j && next_j < N && !flag) {
+        DFS(next_i, next_j, c + 1);
+      }
+    if (flag) return;
+  }
+  board[i][j] = '-';
+  r[i]++;
+  d[j]++;
+  rd[i + (N - j - 1)]++;
+  ld[i + j]++;
+  return;
 }
 
 void solve()
 {
-  FOR(n, N)
-  {
-    FOR(d, 4)
-    {
-      backtracking(n + 1, d);
+  FOR(i, N) {
+    FOR(j, N) {
+      DFS(i, j, 0);
+      if (flag) {
+        return;
+      }
     }
   }
 }
 
 void print()
 {
-  ofstream out("marathon.out");
-  out << "None\n";
+  FOR(i, N) {
+    FOR(j, N) {
+      cout << board[i][j] << ' ';
+    }
+    cout << '\n';
+  }
+  ofstream out("ct.out");
+  FOR(i, N) {
+    FOR(j, N) {
+      out << board[i][j] << ' ';
+    }
+    out << '\n';
+  }
   out.close();
 }
 
