688e49f6886e4cbe4c8ce256227fdd9abcd34eed
회사정현모 <gusah009@naver.com>
2021-06-16 18:16:29 +0900

    최소힙 문제풀이

---
+++ 2021-1/baekjoon/1927_최소힙.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define FOR(i, j) for(int i = 0; i < j; i++)
+
+int N;
+priority_queue<int, vector<int>, greater<int> > pq;
+
+void input()
+{
+  cin >> N;
+  FOR(n, N) {
+    int num;
+    cin >> num;
+    if (num == 0) {
+      if (pq.empty()) {
+        cout << "0\n";
+      } else {
+        cout << pq.top() << '\n';
+        pq.pop();
+      }
+    } else {
+      pq.push(num);
+    }
+  }
+}
+
+void solve()
+{
+}
+
+void print()
+{
+}
+
+int main()
+{
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  input();
+  solve();
+  print();
+}
