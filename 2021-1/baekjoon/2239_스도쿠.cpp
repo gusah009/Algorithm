d35998b7214b1453929ca6900d2c91cfdfbfdd9d
gram_jhm <gusah009@naver.com>
2021-07-03 08:33:24 +0900

    2239 스도쿠 문제풀이

--- 2021-1/baekjoon/2239_스도쿠.cpp
+++ 2021-1/baekjoon/2239_스도쿠.cpp
@@ -3,27 +3,29 @@
 
 using namespace std;
 
-#define FOR(i, j) for(int i = 0; i < j; i++)
+#define FOR(i, j) for (int i = 0; i < j; i++)
 
 int board[9][9];
 bool isExistRow[9][10] = {0};
 bool isExistCol[9][10] = {0};
-bool isExistDiag[9][10][2] = {0};
+bool isExistCell[9][10] = {0};
+
 bool answerFlag = false;
-int X = 0;
 
 void input()
 {
-  FOR(i, 9) {
+  FOR(i, 9)
+  {
     string tmp;
     cin >> tmp;
-    FOR(j, 9) {
+    FOR(j, 9)
+    {
       board[i][j] = tmp[j] - '0';
-      if (board[i][j] != 0) {
+      if (board[i][j] != 0)
+      {
         isExistRow[i][board[i][j]] = true;
         isExistCol[j][board[i][j]] = true;
-        if (i == j) isExistDiag[i][board[i][j]][0] = true;
-        if (i == 8 - j) isExistDiag[i][board[i][j]][1] = true;
+        isExistCell[(i / 3 * 3) + (j / 3)][board[i][j]] = true;
       }
     }
   }
@@ -31,36 +33,32 @@ void input()
 
 void find()
 {
-  FOR(i, 9) {
-    FOR(j, 9) {
-      // if (i == 7 && j == 7) { 
-      //   FOR(i, 9) {
-      //     FOR(j, 9) {
-      //       cout << board[i][j];
-      //     }
-      //     cout << '\n';
-      //   }
-      // }
-      if (board[i][j] == 0) {
-        FOR(n, 9) {
-          if (isExistRow[i][n + 1]) continue;
-          if (isExistCol[j][n + 1]) continue;
-          if (i == j && isExistDiag[i][n + 1][0]) continue;
-          if (i == 8 - j && isExistDiag[i][n + 1][1]) continue;
-          // cout << i << ' ' << j << ' ' << n << '\n';
+  FOR(i, 9)
+  {
+    FOR(j, 9)
+    {
+      if (board[i][j] == 0)
+      {
+        FOR(n, 9)
+        {
+          if (isExistRow[i][n + 1])
+            continue;
+          if (isExistCol[j][n + 1])
+            continue;
+          if (isExistCell[(i / 3 * 3) + (j / 3)][n + 1])
+            continue;
+        
           isExistRow[i][n + 1] = true;
           isExistCol[j][n + 1] = true;
-          if (i == j) isExistDiag[i][n + 1][0] = true;
-          if (i == 8 - j) isExistDiag[i][n + 1][1] = true;
+          isExistCell[(i / 3 * 3) + (j / 3)][n + 1] = true;
           board[i][j] = n + 1;
           find();
-          X++;
-          if (answerFlag) return;
+          if (answerFlag)
+            return;
           board[i][j] = 0;
           isExistRow[i][n + 1] = false;
           isExistCol[j][n + 1] = false;
-          if (i == j) isExistDiag[i][n + 1][0] = false;
-          if (i == 8 - j) isExistDiag[i][n + 1][1] = false;
+          isExistCell[(i / 3 * 3) + (j / 3)][n + 1] = false;
         }
         return;
       }
@@ -72,28 +70,32 @@ void find()
 
 void solve()
 {
-  FOR(i, 9) {
-    FOR(j, 9) {
-      if (board[i][j] == 0) {
-        FOR(n, 9) {
-          if (isExistRow[i][n + 1]) continue;
-          if (isExistCol[j][n + 1]) continue;
-          if (i == j && isExistDiag[i][n + 1][0]) continue;
-          if (i == 8 - j && isExistDiag[i][n + 1][1]) continue;
-          cout << i << ' ' << j << ' ' << n + 1 << '\n';
+  FOR(i, 9)
+  {
+    FOR(j, 9)
+    {
+      if (board[i][j] == 0)
+      {
+        FOR(n, 9)
+        {
+          if (isExistRow[i][n + 1])
+            continue;
+          if (isExistCol[j][n + 1])
+            continue;
+          if (isExistCell[(i / 3 * 3) + (j / 3)][n + 1])
+            continue;
+
           isExistRow[i][n + 1] = true;
           isExistCol[j][n + 1] = true;
-          if (i == j) isExistDiag[i][n + 1][0] = true;
-          if (i == 8 - j) isExistDiag[i][n + 1][1] = true;
+          isExistCell[(i / 3 * 3) + (j / 3)][n + 1] = true;
           board[i][j] = n + 1;
           find();
-          X++;
-          if (answerFlag) return;
+          if (answerFlag)
+            return;
           board[i][j] = 0;
           isExistRow[i][n + 1] = false;
           isExistCol[j][n + 1] = false;
-          if (i == j) isExistDiag[i][n + 1][0] = false;
-          if (i == 8 - j) isExistDiag[i][n + 1][1] = false;
+          isExistCell[(i / 3 * 3) + (j / 3)][n + 1] = false;
         }
         return;
       }
@@ -103,18 +105,19 @@ void solve()
 
 void print()
 {
-  FOR(i, 9) {
-    FOR(j, 9) {
+  FOR(i, 9)
+  {
+    FOR(j, 9)
+    {
       cout << board[i][j];
     }
     cout << '\n';
   }
-  cout << X << ' ' << answerFlag;
 }
 
 int main()
 {
-  ios_base :: sync_with_stdio(false);
+  ios_base ::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
   input();
